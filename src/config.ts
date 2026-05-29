f5aa41f9acc21df0ad4b83e724773aafb72347c5
anishtiw16 <anishtiwari@clinikally.com>
2026-05-29 17:36:10 +0530

    feat: add prod link for microservice

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,6 @@ export const WHATSAPP_PHONE_NUMBER_ID = process.env.WHATSAPP_PHONE_NUMBER_ID;
 export const DERMAGPT_API_KEY = process.env.DERMAGPT_API_KEY || 'dermagptsecretkey123#';
 
 export const META_API_URL = 'https://graph.facebook.com';
-export const CHAT_API_URL = 'https://eufemia-discriminantal-linearly.ngrok-free.dev/api/v1/chat';
-export const UPLOAD_FILE_URL = 'https://eufemia-discriminantal-linearly.ngrok-free.dev/api/v1/upload';
+export const CHAT_API_URL = 'https://waap-mservice.onrender.com/api/v1/chat';
+export const UPLOAD_FILE_URL = 'https://waap-mservice.onrender.com/api/v1/upload';
 export const META_API_VERSION = 'v20.0';
